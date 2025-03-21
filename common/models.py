9c586ecaa3bce18fb56875ac348c0d755dc9fe57
Chenxu Wang <117066286+vcis-wangchenxu@users.noreply.github.com>
2025-03-21 21:58:17 +0800

    Add DuelingNet

--- common/models.py
+++ common/models.py
@@ -52,3 +52,46 @@ class Qnet_back(nn.Module):
         x = x.view(x.size(0), -1)  # Flatten the tensor
         x = torch.relu(self.fc1(x))
         return self.fc2(x)
+
+
+class DuelingNet(nn.Module):
+    """ CNN处理obs 
+    define: state_shape[h], state_shape[w], action_dim
+    input : [batch_size, 3, h, w]
+    output: [batch_size, action_dim]
+    """
+    def __init__(self, h, w, outputs):
+        super(DuelingNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.bn1   = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2   = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.bn3   = nn.BatchNorm2d(32)
+
+        # 计算卷积层输出尺寸
+        def conv2d_size_out(size, kernel_size=3, stride=1, padding=1):
+            return (size-kernel_size+2*padding) // stride + 1
+        
+        # 假设初始输入为(h, w)，依次计算经过每一层卷积后的输出尺寸
+        convw, convh = w, h
+        for _ in range(3):  # 因为有三层卷积
+            convw = conv2d_size_out(convw)
+            convh = conv2d_size_out(convh)
+        
+        linear_input_size = convw * convh * 32  # 最终的线性层输入大小
+        self.head_A         = nn.Linear(linear_input_size, 256)
+        self.head_advantage = nn.Linear(256, outputs)
+        self.head_V         = nn.Linear(linear_input_size, 256)
+        self.head_value     = nn.Linear(256, 1)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        advantage_A = F.relu(self.head_A(x.view(x.size(0), -1)))
+        advantage   = self.head_advantage(advantage_A)
+        value_V     = F.relu(self.head_V(x.view(x.size(0), -1)))
+        value       = self.head_value(value_V)
+
+        return value + advantage - advantage.mean(dim=1, keepdim=True)   # [batch_size, 1] + [batch_size, action_dim] - [batch_size, 1]
